bf5ff78053e0a24d241a7a2486be101d6a49b46d
Bishal Adhikari <winnerbishal9848@gmail.com>
2023-08-12 22:14:29 +0545

    Few canges

--- updated_streamlit_app.py
+++ updated_streamlit_app.py
@@ -31,7 +31,7 @@ x_data, target, scaler_target = preprocess_data(df_stock)
 
 # User input for 'Open', 'Moving_Avg', and 'RSI' with default values
 user_input_open = st.number_input("Enter 'Open' price:", min_value=0.0, step=0.01, value=default_open)
-user_input_moving_avg = st.number_input("Enter 'Moving Average':", min_value=0.0, step=0.01, value=default_moving_avg)
+user_input_moving_avg = st.number_input("Enter 'Five-day Moving Average':", min_value=0.0, step=0.01, value=default_moving_avg)
 user_input_rsi = st.number_input("Enter 'RSI' value:", min_value=0.0, max_value=100.0, step=0.01, value=default_rsi)
 
 # Create a feature array from user input
@@ -52,8 +52,8 @@ model_weights_path = f"models/{selected_stock}_{model_type}.h5"
 model.load_weights(model_weights_path)
 
 # Predict using the last sequence from data + user input
-input_sequence = np.concatenate((x_data[-1][1:], user_input_features)).reshape(1, x_data[0].shape[0], x_data[0].shape[1])
+input_sequence = np.concatenate((np.zeros(x_data[-1][1:].shape), user_input_features)).reshape(1, x_data[0].shape[0], x_data[0].shape[1])
 prediction = model.predict(input_sequence)
 predicted_price = scaler_target.inverse_transform(prediction)[0][0]
 
-st.write(f"Predicted Closing Price for {selected_stock} using {model_type}: {predicted_price:.2f}")
+st.markdown(f"Predicted Closing Price for {selected_stock} using {model_type}: **:red[Rs. {predicted_price:.2f}]**")
